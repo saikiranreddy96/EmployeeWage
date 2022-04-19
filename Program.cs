0bfe54fbb56e0d55f178cfc698ced5a07387f7c3
saikiranreddy <saikiranreddy@gmail.com>
2022-04-19 13:45:37 +0530

    [kiran]-Calculate EmployeeWage

--- Program.cs
+++ Program.cs
@@ -10,15 +10,29 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int isPresent = 1;
+            //Constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-
-            int empInput = random.Next(0, 2);
-            if (isPresent == empInput)
-                Console.WriteLine("Employee is present");
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee is absent");
-                Console.ReadLine();
+            {
+                empHrs = 0;
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+
+            Console.ReadLine();
+
         }
     }
 }
