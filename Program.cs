427b288eb62b5b27714a7aaa7ba626bf415f610d
saikiranreddy <saikiranreddy@gmail.com>
2022-04-19 13:58:28 +0530

    [kiran]-Calculate EmployeeWage

--- Program.cs
+++ Program.cs
@@ -11,28 +11,31 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             //Constants
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
             else
             {
                 empHrs = 0;
-
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
+
             Console.WriteLine("Emp Wage :" + empWage);
-
             Console.ReadLine();
-
         }
-    }
+    } 
 }
